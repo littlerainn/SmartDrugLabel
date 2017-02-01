49e6cabef07fa12ca236a679917b4ab0b0c867aa
fon <little.rainn@gmail.com>
2017-02-01 15:50:59 +0700

    Finish 1Feb2017

--- app/src/main/java/com/smartdruglabel/smartdruglabel/ChangePwdActivity.java
+++ app/src/main/java/com/smartdruglabel/smartdruglabel/ChangePwdActivity.java
@@ -37,6 +37,7 @@ import android.widget.Toast;
 
 public class ChangePwdActivity extends Activity {
     @SuppressLint("NewApi")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
